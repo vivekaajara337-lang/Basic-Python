64f9e6342147252a92bd2b1e8809f7839ae9cf98
vivekaajara337-lang <vivekaajara337@gmail.com>
2026-06-27 00:36:50 +0530

    Reformat SpecificMethods.py for better readability

--- SpecificMethods.py
+++ SpecificMethods.py
@@ -22,4 +22,29 @@ print(b.isalpha())
 
 print(a.isprintable())
 
+#Tuple
+a = ("Vivek","Aajara",10,5,"hi")
+print(a.count("V"))
+print(a.count(5))
+
+print(len(a))
+
+list = [0,1,2]
+print(tuple(list))
+
+print(tuple("Vivek"))
+
+# t = (10, 20, 30, 40)
+# lst = list(t)
+# lst[2] = 99
+# t = tuple(lst)
+# print(t)
+
+del a
+print(a)
+
+#List
+a = ["aaple","banana","cherry"]
+a.append("orange")
+print(a)
 
