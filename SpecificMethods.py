89bcce9ac728c212d1c66931585cc583a41f77ca
vivekaajara337-lang <vivekaajara337@gmail.com>
2026-06-26 22:37:09 +0530

    Add files via upload

---
+++ SpecificMethods.py
@@ -0,0 +1,25 @@
+#String:
+a = "Vivek"
+print(a.find("ek"))
+
+print(a.replace("ek", "ka"))
+
+print(a.upper())
+
+print(a.lower())
+
+b= "Nitin"
+print(b.count("i"))
+print(a.count("v"))
+
+print(b.islower())
+
+print(b.isupper())
+
+print(b.isnumeric())
+
+print(b.isalpha())
+
+print(a.isprintable())
+
+
