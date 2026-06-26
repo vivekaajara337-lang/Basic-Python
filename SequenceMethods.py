264ee020f54d2b0c2777684b4538d9c6ef7d13dc
vivekaajara337-lang <vivekaajara337@gmail.com>
2026-06-27 00:36:01 +0530

    Refactor SequenceMethods.py to include tuples and lists

--- SequenceMethods.py
+++ SequenceMethods.py
@@ -13,3 +13,40 @@ print(b)
 
 print(a[0])
 print(a[-1])
+
+#Tuple
+a = ("hi",1)
+b = (22.7,20)
+c = a + b
+print (c)
+
+a = ("Vivek","Aajara",10,5,"hi")
+print(a[0])
+print(a[1])
+print(a[-1])
+
+b = a[3] * 3
+print(b)
+b = a[2] + a[3]
+print(b)
+
+print(a[0:2])
+print(a[-3:-1])
+
+#List
+a = ["Vivek","Aajara",10,5,"hi"]
+print(a[0])
+print(a[1])
+print(a[-1])
+
+print(a[0:2])
+print(a[-3:-1])
+
+b = a*2
+print(b)
+
+a = ["hi",1]
+b = [22.7,20]
+c = a + b
+print (c)
+
